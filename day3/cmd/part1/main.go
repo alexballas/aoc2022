37777121e170dd9647e0db2c7e85fa86fb46d78d
Alex Ballas <alex@ballas.org>
2022-12-03 22:10:38 +0200

    Day 3

---
+++ day3/cmd/part1/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"unicode"
+)
+
+func main() {
+	f, _ := os.Open("input.txt")
+	defer f.Close()
+
+	buf := bufio.NewScanner(f)
+
+	var priority int32
+
+	for buf.Scan() {
+		line := buf.Text()
+		word1 := line[:len(line)/2]
+		word2 := line[len(line)/2:]
+
+		var common rune
+	out:
+		for _, a := range word1 {
+			for _, b := range word2 {
+				if a == b {
+					common = a
+					break out
+				}
+			}
+		}
+
+		priority += prio(common)
+	}
+
+	fmt.Println("Part 1:", priority)
+
+}
+
+func prio(a rune) rune {
+	if a >= 'A' && a <= 'Z' {
+		a = unicode.ToLower(a) - 'A' - 5
+		return a
+	}
+
+	if a >= 'a' && a <= 'z' {
+		a = unicode.ToUpper(a) - 'A' + 1
+		return a
+	}
+
+	return -1
+}
---
+++ day3/cmd/part2/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"unicode"
+)
+
+func main() {
+	f, _ := os.Open("input.txt")
+	defer f.Close()
+
+	buf := bufio.NewScanner(f)
+
+	var priority int32
+
+	bufSlice := make([]string, 3)
+	var counter int
+
+	for buf.Scan() {
+		var common rune
+
+		switch counter % 3 {
+		case 0:
+			bufSlice[0] = buf.Text()
+		case 1:
+			bufSlice[1] = buf.Text()
+		case 2:
+			bufSlice[2] = buf.Text()
+
+		out:
+			for _, a := range bufSlice[0] {
+				for _, b := range bufSlice[1] {
+					for _, c := range bufSlice[2] {
+						if a == b && a == c {
+							common = a
+							break out
+						}
+					}
+				}
+			}
+
+			priority += prio(common)
+		}
+		counter++
+	}
+
+	fmt.Println("Part 2:", priority)
+
+}
+
+func prio(a rune) rune {
+	if a >= 'A' && a <= 'Z' {
+		a = unicode.ToLower(a) - 'A' - 5
+		return a
+	}
+
+	if a >= 'a' && a <= 'z' {
+		a = unicode.ToUpper(a) - 'A' + 1
+		return a
+	}
+
+	return -1
+}
