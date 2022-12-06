64d6588447b0e65ccc577586f7d33864c46a7d02
Alex Ballas <alex@ballas.org>
2022-12-06 21:30:47 +0200

    Day 6

---
+++ day6/cmd/part2/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	f, _ := os.Open("input.txt")
+	defer f.Close()
+
+	buf := bufio.NewScanner(f)
+
+	var result int
+	fmt.Println()
+	for buf.Scan() {
+		line := buf.Text()
+
+		for i := 0; i < len(line)-13; i++ {
+			fourChars := line[i : i+14]
+			uniq := make(map[rune]struct{})
+			for _, letter := range fourChars {
+				uniq[letter] = struct{}{}
+			}
+
+			if len(uniq) == 14 {
+				result = i + 14
+				fmt.Println("Part 2:", result)
+				break
+			}
+		}
+	}
+}
